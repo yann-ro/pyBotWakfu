185876fc008b36b5cf4b555e0521575610ed0e4d
Yann Rodriguez <yann.rodriguez@wanadoo.fr>
2020-11-07 16:04:07 +0100

    add datas.py

---
+++ Datas.py
@@ -0,0 +1,17 @@
+
+# Data
+
+zoneCapChat = (940,530, 1310, 750)
+zoneMineur = (630, 360, 1000, 600)
+
+middle =(960,550)
+downLeft =(130,960)
+left=(130,550)
+upLeft=(130,130)
+up = (930,130)
+upRight = (1800,130)
+right = (1800,530)
+downRight =(1800,960)
+
+pionDim =(20,34)
+mapDim = (580, 300,1305,792)
